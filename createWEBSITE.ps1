c2ec0d0e47a82436b61a992ab41030e7631fd27a
norman2688 <normananos26@gmail.com>
2024-09-09 15:59:38 +0800

    Update createWEBSITE.ps1

--- createWEBSITE.ps1
+++ createWEBSITE.ps1
@@ -1,3 +1,3 @@
 ﻿#creating web apps
-New-Website -name "ccna22 website" -hostheader "www.ccna22.com" -physicalpath "c:\webs\datingbiz"
+New-Website -name "ccna22 website" -hostheader "www.ccna22.com" -physicalpath "d:\webs\datingbiz"
 						   
